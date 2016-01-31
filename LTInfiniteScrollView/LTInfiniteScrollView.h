c86056e061b2750715526612cb4e2e969cd613b2
Terry Kwon <terry.kwon@digiflare.com>
2016-01-31 16:17:27 -0500

    add framework

---
+++ LTInfiniteScrollView/LTInfiniteScrollView.h
@@ -0,0 +1,19 @@
+//
+//  LTInfiniteScrollView.h
+//  LTInfiniteScrollView
+//
+//  Created by Terry Kwon on 2016-01-31.
+//  Copyright © 2016 io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LTInfiniteScrollView.
+FOUNDATION_EXPORT double LTInfiniteScrollViewVersionNumber;
+
+//! Project version string for LTInfiniteScrollView.
+FOUNDATION_EXPORT const unsigned char LTInfiniteScrollViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LTInfiniteScrollView/PublicHeader.h>
+
+
